cba7eba83a5d72ae1f07e73f9ad6c19759fa2259
Alex <alexyuwen@gmail.com>
2021-03-17 21:54:05 -0700

    improved error messages

--- main.py
+++ main.py
@@ -15,36 +15,42 @@ def main():
     PATH = "/Users/alexyuwen/myasu-webscraper/chromedriver"
     driver = webdriver.Chrome(PATH)
     driver.get("https://webapp4.asu.edu/catalog/classlist?t=2207&hon=F&promod=F&e=open&page=1")
-    sleep(4)  # wait for page to load since MyAsu's class search page can be buggy sometimes
+    sleep(4)  # wait for page to load since MyASU's class search page can be buggy sometimes
     repeatingInterval = 15  # in seconds
     courses = {}  # Dict[classNum: seatsOpen]
     try:
-        minutesToRun = int(sys.argv[1])
+        minutesToRun = int(sys.argv[3])
         timeEnd = time() + 60 * minutesToRun
     except IndexError:
         timeEnd = float("inf")  # positive infinity
 
     updateCoursesList(driver, courses, initialized=False)
-    while time() < timeEnd:  # Todo: take command line argument of total minutes to run so that program doesn't run indefinitely
+    while time() < timeEnd:
         updateCoursesList(driver, courses)
         sleep(repeatingInterval - (time() % repeatingInterval))
 
 def updateCoursesList(driver, courses, initialized=True):
+    try:
+        subject = sys.argv[1]
+        number = sys.argv[2]
+    except:
+        print("Either you did not provide command-line arguments or the ones you provided are invalid.")
+
     select = Select(driver.find_element_by_id("term"))
-    select.select_by_visible_text('Fall 2020')
+    select.select_by_visible_text('Fall 2021')
 
     search = driver.find_element_by_id("subjectEntry")
-    search.send_keys("CSE")
+    search.send_keys(subject)
 
     search = driver.find_element_by_id("catNbr")
     search.clear()
-    search.send_keys("340")
+    search.send_keys(number)
     search.send_keys(Keys.RETURN)
     sleep(3)  # wait for results to load
 
     try:
-        mytable = driver.find_element_by_id('CatalogList')
-        rows = mytable.find_elements_by_css_selector('tr')[1:]  # excludes first element, which is an empty list
+        table = driver.find_element_by_id('CatalogList')
+        rows = table.find_elements_by_css_selector('tr')[1:]  # excludes first element, which is an empty list
     except NoSuchElementException:
         rows = []
     except:
@@ -54,14 +60,23 @@ def updateCoursesList(driver, courses, initialized=True):
     for row in rows:
         classNum = row.find_elements_by_tag_name('td')[2].text
         seatsOpen = row.find_elements_by_tag_name('td')[10].text
+        teacher = row.find_elements_by_tag_name('td')[3].text
         if classNum not in courses:
             courses[classNum] = seatsOpen
             if initialized:
-                print("A new course has been added.")
+                print("Either a new course has been added or a new spot for an existing course has opened.")
                 call(["afplay", "notification_sound.wav"])  # play notification sound
         elif courses.setdefault(classNum, seatsOpen) != seatsOpen and initialized:
-            print("The number of open seats has changed.")
+            print(f"The number of open seats in {teacher}'s section has changed from {courses[classNum]} to {seatsOpen}.")
             call(["afplay", "notification_sound.wav"])  # play notification sound
+            courses[classNum] = seatsOpen
+
+        # # for debugging
+        # i = -1
+        # for cell in row.find_elements_by_tag_name('td'):
+        #     i += 1
+        #     print(f"{i:4}| {cell.text}")
+
     # print(courses)
 
 
